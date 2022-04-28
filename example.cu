ffb40778615aac557cd893af333bcada4675ca7c
Ken <odessa5d4@gmail.com>
2022-04-29 02:19:44 +0900

    test code update

--- example.cu
+++ example.cu
@@ -1,6 +1,7 @@
 #include"CUMemory.cuh"
 #include<iostream>
 #include<Windows.h>
+
 __global__ void memoryChecker()
 {
 
@@ -21,37 +22,11 @@ void WriteMemory(void* vtr, _Ty data)
 
 int main()
 {
-	void* ptr = malloc(4);
-
-	int* intPtr = reinterpret_cast<int*>(ptr);
-
-	free(ptr);
+	CUMemory mem(1024);
 	
-	CUMemory memory(1024);
-
-	CUMemory memory2(512);
-	
-	void* devicePtr0 = memory.Alloc(4);
-	void* devicePtr1 = memory.Alloc(16);
-	void* devicePtr2 = memory.Alloc(32);
-	void* devicePtr3 = memory.Alloc(8);
-	void* devicePtr4 = memory.Alloc(16);
-	void* devicePtr5 = memory.Alloc(4);
-
-	void* devicePtr6 = memory2.Alloc(32);
-
-	void* devicePtr7 = memory.Alloc(4);
-
-	int src = 17;
-	int data;
-
-	WriteMemory<int>(devicePtr7, src);
-
-	CopyDeivceToHost<int>(devicePtr7, &data, 1);
-
-	memoryChecker << <1, 1 >> > ();
-
-	memory.Release(devicePtr3);
-
+	void* ptr0 = mem.Alloc(4);
+	void* ptr1 = mem.Alloc(16);
+	mem.Release(ptr0);
+	void* ptr2 = mem.Alloc(2);
 	return 0;
 }
